fbedc7bd77eb1c1581b0379d230ffbaf77978b32
dogeplusplus <albertswchung@gmail.com>
2022-02-26 23:15:12 +0000

    Adding basic transformer network.

--- transformers_from_scratch/example.py
+++ transformers_from_scratch/example.py
@@ -48,3 +48,57 @@ class SelfAttention(nn.Module):
 
         out = out.transpose(1, 2).contiguous().view(b, t, h*k)
         return self.unify_heads(out)
+
+
+layer = SelfAttention(k)
+out = layer(x)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, k, heads):
+        super().__init__()
+
+        self.attention = SelfAttention(k, heads=heads)
+
+        self.norm1 = nn.LayerNorm(k)
+        self.norm2 = nn.LayerNorm(k)
+
+        self.ff = nn.Sequential(
+            nn.Linear(k, 4*k),
+            nn.ReLU(),
+            nn.Linear(4*k, k)
+        )
+
+    def forward(self, x):
+        attended = self.attention(x)
+        x = self.norm1(attended + x)
+
+        fedforward = self.ff(x)
+        return self.norm2(fedforward + x)
+
+class Transformer(nn.Module):
+    def __init__(self, k, heads, depth, seq_length, num_tokens, num_classes):
+        super().__init__()
+
+        self.num_tokens = num_tokens
+        self.token_emb = nn.Embedding(num_tokens, k)
+        self.pos_emb = nn.Embedding(seq_length, k)
+
+        tblocks = []
+        for i in range(depth):
+            tblocks.append(TransformerBlock(k, heads))
+        self.tblocks = nn.Sequential(*tblocks)
+
+        self.to_probs = nn.Linear(k, num_classes)
+
+    def forward(self, x):
+        tokens = self.token_emb(x)
+        b, t, k = tokens.size()
+
+        positions = torch.arange(t)
+        positions = self.pos_emb(positions)[None, :, :].expand(b, t, k)
+
+        x = tokens + positions
+        x = self.tblocks(x)
+
+        x = self.to_probs(x.mean(dim=1))
+        return F.log_softmax(x, dim=1)

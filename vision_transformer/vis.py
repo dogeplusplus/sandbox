5713f1aec37b84a27b38380761ca1d9bed989aad
dogeplusplus <albertswchung@gmail.com>
2022-03-05 23:14:06 +0000

    Visual transformer using haiku and einsops (WIP).

--- vision_transformer/vis.py
+++ vision_transformer/vis.py
@@ -1,20 +1,13 @@
 import jax
+import optax
 import haiku as hk
-from jax import random
 import jax.numpy as jnp
-from einops import rearrange
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
+from jax import random
+from einops import rearrange, repeat, reduce
 
-H = 256
-W = 256
-C = 3
-
-P = 16
-N = H * W / P ** 2
-
-key = random.PRNGKey(42)
-image = jax.random.normal(key, (H, W, C))
-reshaped = rearrange(image, "(h p1) (w p2) c -> (h w) p1 p2 c", p1=P, p2=P)
 
 class SelfAttention(hk.Module):
     def __init__(self, k, heads=8):
@@ -22,12 +15,18 @@ class SelfAttention(hk.Module):
         self.k = k
         self.heads = heads
 
+        self.to_queries = hk.Linear(k*heads, with_bias=False)
+        self.to_keys = hk.Linear(k*heads, with_bias=False)
+        self.to_values = hk.Linear(k*heads, with_bias=False)
+        self.unify_heads = hk.Linear(k)
+
     def __call__(self, x):
         h = self.heads
+        k = self.k
 
-        queries = rearrange(hk.Linear(k*h, with_bias=False)(x), "b t (k h)  -> (b h) t k", h=h)
-        keys = rearrange(hk.Linear(k*h, with_bias=False)(x), "b t (k h) -> (b h) t k", h=h)
-        values = rearrange(hk.Linear(k*h, with_bias=False)(x), "b t (k h) -> (b h) t k", h=h)
+        queries = rearrange(self.to_queries(x), "b t (k h)  -> (b h) t k", h=h)
+        keys = rearrange(self.to_keys(x), "b t (k h) -> (b h) t k", h=h)
+        values = rearrange(self.to_values(x), "b t (k h) -> (b h) t k", h=h)
 
         queries = queries / (k ** (1/4))
         keys = keys / (k ** (1/4))
@@ -36,12 +35,36 @@ class SelfAttention(hk.Module):
         dot = jax.nn.softmax(dot, axis=2)
 
         out = rearrange(jax.lax.batch_matmul(dot, values), "(b h) t k -> b t (h k)", h=h)
-        unify_heads = hk.Linear(k)(out)
+        attention = self.unify_heads(out)
 
-        return unify_heads
+        return attention
+
 
 class TransformerBlock(hk.Module):
-    def __init__(self, k, heads, depth, seq_length, num_tokens, num_classes):
+    def __init__(self, k, heads):
+        self.k = k
+        self.heads = heads
+
+        self.attention = SelfAttention(self.k, self.heads)
+        self.layer_norm_1 = hk.LayerNorm(axis=[-2, -1])
+        self.forward = hk.Sequential(
+            hk.Linear(4*self.k),
+            hk.ReLU(),
+            hk.Linear(self.k),
+        )
+        self.layer_norm_2 = hk.LayerNorm(axis=[-2, -1])
+
+
+    def __call__(self, x):
+        attended = self.attention(x)
+        x = self.layer_norm_1(attended + x)
+        forward = self.forward(x)
+        out = self.layer_norm_2(forward + x)
+        return out
+
+
+class VisionTransformer(hk.Module):
+    def __init__(self, k, heads, depth, seq_length, num_tokens, num_classes, patch_size):
         super().__init__()
         self.k = k
         self.heads = heads
@@ -49,25 +72,79 @@ class TransformerBlock(hk.Module):
         self.seq_length = seq_length
         self.num_tokens = num_tokens
         self.num_classes = num_classes
+        self.patch_size = patch_size
+
+
+        self.pos_emb = hk.Embed(self.seq_length, self.k)
+        self.token_emb = hk.Embed(self.num_tokens, self.k)
+        self.blocks = hk.Sequential([
+            TransformerBlock(self.k, self.heads) for _ in range(self.depth)
+        ])
+        self.classification = hk.Linear(self.num_classes)
 
     def __call__(self, x):
-        token_emb = hk.Embed()
-        pos_emb = hk.Embed()
+        batch_size = x.shape[0]
+
+        positions = jnp.arange(self.seq_length)
+        positions = repeat(self.pos_emb(positions), "t k -> b t k", b=batch_size)
+
+        x = rearrange(x, "b (h p1) (w p2) c -> b (h w) p1 p2 c", p1=self.patch_size, p2=self.patch_size)
+        tokens = self.token_emb(x)
+
+        x = tokens + positions
+        x = self.blocks(x)
+        x = self.classification(x)
+        x = reduce(x, "b t c -> b c", "mean")
+
+        return jax.nn.log_softmax(x, axis=1)
 
 
-b = 3
-t = 5
-k = 7
+@jax.jit
+def predict(params, image, k, heads, depth, seq_length, num_tokens, num_classes, patch_size):
+    transformer = VisionTransformer(
+        k,
+        heads,
+        depth,
+        seq_length,
+        num_tokens,
+        num_classes,
+        patch_size,
+    )
 
-x = jnp.ones((b, t, k))
 
-def _attention(x):
-    layer = SelfAttention(k)
-    return layer(x)
+def main():
+    b = 3
+    t = 5
+    k = 7
 
-attention = hk.transform(_attention)
+    x = jnp.ones((b, t, k))
 
-rng = random.PRNGKey(0)
-params = attention.init(rng, x=x)
-y = attention.apply(params=params, x=x, rng=rng)
-import pdb; pdb.set_trace()
+    def _attention(x):
+        layer = SelfAttention(k)
+        return layer(x)
+
+    attention = hk.transform(_attention)
+
+    rng = random.PRNGKey(0)
+    params = attention.init(rng, x=x)
+    y = attention.apply(params=params, x=x, rng=rng)
+
+    batch_size = 16
+    epochs = 100
+
+    train_ds, val_ds = tfds.load(
+        "food101",
+        split=["train", "test"],
+        shuffle_files=True,
+    )
+    train_ds = train_ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+
+    for e in range(epochs):
+        for xs, ys in train_ds:
+            grads = jax.grad(loss)(params, xs, ys)
+            updates, opt_state = optimizer.update(grads, opt_state)
+            params = optax.apply_updates(params, updates)
+
+
+

761b833f182447c936a817915b70a404efc31c07
dogeplusplus <albertswchung@gmail.com>
2022-03-10 23:16:37 +0000

    Trying to change the embedding layer.

--- vision_transformer/vis.py
+++ vision_transformer/vis.py
@@ -1,10 +1,12 @@
 import jax
 import optax
+import numpy as np
 import typing as t
 import haiku as hk
+import jax.nn as nn
 import jax.numpy as jnp
-# import tensorflow as tf
-# import tensorflow_datasets as tfds
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
 from tqdm import trange
 from jax import random
@@ -92,12 +94,10 @@ class VisionTransformer(hk.Module):
         self.pos_emb = hk.Embed(seq_len, self.k)
 
     def __call__(self, x):
-        batch_size = x.shape[0]
-
         x = rearrange(x, "b (h p1) (w p2) c -> b (h w) (p1 p2 c)", p1=self.patch_size, p2=self.patch_size)
         tokens = self.token_emb(x)
         # Position embedding is regular embedding mapping index to (seq_length,) to (seq_length, k)
-        positions = repeat(jnp.arange(self.seq_len, dtype=jnp.int32), "t -> b t", b=batch_size)
+        positions = repeat(jnp.arange(self.seq_len, dtype=jnp.int32), "t -> 1 t")
         positions = self.pos_emb(positions)
 
         x = tokens + positions
@@ -108,83 +108,99 @@ class VisionTransformer(hk.Module):
         return jax.nn.log_softmax(x, axis=1)
 
 
+def sinusoidal_init(max_len, min_scale=1.0, max_scale=10000.0):
+    def init(key, shape, dtype=np.float32):
+        del key, dtype
+        d_feature = shape[-1]
+        pe = np.zeros((max_len, d_feature), dtype=np.float32)
+        position = rearrange(np.arange(0, max_len), "p -> p 1")
+        scale_factor = -np.log(max_scale / min_scale) / (d_feature // 2 - 1)
+        div_term = min_scale * np.exp(np.arange(0, d_feature // 2) * scale_factor)
+        pe[:, :d_feature // 2] = np.sin(position * div_term)
+        pe[:, d_feature // 2: 2 * (d_feature // 2)] = np.cos(position * div_term)
+        pe = rearrange(pe, "p -> 1 p")
+
+        return jnp.array(pe)
+
+    return init
+
+class SinPosEmb(hk.Module):
+    def __call__(self, x):
+        embedding = hk.get_parameter("pos_emb", [], init=sinusoidal_init)
+        return embedding + x
+
 def main():
-    batch_size = 4
+    batch_size = 2
     epochs = 100
     num_classes = 101
 
-    # train_ds, val_ds = tfds.load(
-    #     "food101",
-    #     split=["train", "validation"],
-    #     shuffle_files=True,
-    # )
-    # def resize(example):
-    #     image = tf.image.resize(example["image"], [128, 128])
-    #     label = example["label"]
-    #     image = tf.cast(image, tf.float32)
+    train_ds, val_ds = tfds.load(
+        "food101",
+        split=["train", "validation"],
+        shuffle_files=True,
+    )
+    def resize(example):
+        image = tf.image.resize(example["image"], [32, 32])
+        label = example["label"]
+        image = tf.cast(image, tf.float32)
 
-    #     return image, label
+        return image, label
 
-    # train_ds = train_ds.map(resize).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-    # train_ds = tfds.as_numpy(train_ds)
-    # val_ds = tfds.as_numpy(val_ds)
+    train_ds = train_ds.map(resize).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    train_ds = tfds.as_numpy(train_ds)
+    val_ds = tfds.as_numpy(val_ds)
 
-    # def create_transformer(x):
-    #     return VisionTransformer(
-    #         k=128,
-    #         heads=8,
-    #         depth=2,
-    #         num_tokens=5,
-    #         num_classes=101,
-    #         patch_size=32,
-    #         seq_len=64,
-    #     )(x)
+    def create_transformer(x):
+        return VisionTransformer(
+            k=128,
+            heads=8,
+            depth=2,
+            num_tokens=5,
+            num_classes=101,
+            patch_size=8,
+            seq_len=64,
+        )(x)
 
-    # model = hk.transform(create_transformer)
-    # transformer = hk.without_apply_rng(model)
+    model = hk.transform(create_transformer)
+    transformer = hk.without_apply_rng(model)
 
     # xs, _ = next(iter(train_ds))
-    # rng = random.PRNGKey(42)
-    # params = transformer.init(rng, xs)
 
+    xs = jnp.ones((2, 32, 32, 3))
 
-    def create_attn(x):
-        return SelfAttention(128, 8)(x)
-
-    model = hk.transform(create_attn)
     rng = random.PRNGKey(42)
-    xs = jnp.ones((64, 32, 128))
-    params = model.init(rng, xs)
-    param_count = sum(x.size for x in jax.tree_leaves())
+    params = transformer.init(rng, xs)
+    param_count = sum(x.size for x in jax.tree_leaves(params))
+    import pdb; pdb.set_trace()
 
-    tx = optax.adam(learning_rate=3e-4)
-    opt_state = tx.init(params)
+    # tx = optax.adam(learning_rate=3e-4)
+    # opt_state = tx.init(params)
 
-    def loss_fn(params, xs, ys):
-        logits = transformer.apply(params, xs)
-        one_hot = jax.nn.one_hot(ys, num_classes=num_classes)
-        return optax.softmax_cross_entropy(logits, one_hot).sum()
+    # def loss_fn(params, xs, ys):
+    #     logits = transformer.apply(params, xs)
+    #     one_hot = jax.nn.one_hot(ys, num_classes=num_classes)
+    #     return optax.softmax_cross_entropy(logits, one_hot).sum()
 
-    @jax.jit
-    def update(
-        params: hk.Params,
-        opt_state: optax.OptState,
-        xs: tf.Tensor,
-        ys: tf.Tensor
-    ) -> t.Tuple[hk.Params, optax.OptState]:
-        grads = jax.grad(loss_fn)(params, xs, ys)
-        updates, opt_state = tx.update(grads, opt_state)
-        new_params = optax.apply_updates(params, updates)
+    # @jax.jit
+    # def update(
+    #     params: hk.Params,
+    #     opt_state: optax.OptState,
+    #     xs: tf.Tensor,
+    #     ys: tf.Tensor
+    # ) -> t.Tuple[hk.Params, optax.OptState]:
+    #     grads = jax.grad(loss_fn)(params, xs, ys)
+    #     updates, opt_state = tx.update(grads, opt_state)
+    #     new_params = optax.apply_updates(params, updates)
 
-        return new_params, opt_state
+    #     return new_params, opt_state
 
-    pbar = trange(epochs)
-    for _ in pbar:
-        step = 0
-        for xs, ys in train_ds:
-            params, opt_state = update(params, opt_state, xs, ys)
-            pbar.update(step)
-            step += 1
+    # pbar = trange(epochs)
+    # for _ in pbar:
+    #     step = 0
+    #     for xs, ys in train_ds:
+    #         params, opt_state = update(params, opt_state, xs, ys)
+    #         pbar.update(step)
+    #         step += 1
 
 if __name__ == "__main__":
     main()

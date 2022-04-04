bb164eaacc56e1dd731d5858ef841cd7a7154ea3
Albert Chung <albertswchung@gmail.com>
2022-04-04 21:04:09 +0100

    Add basic structure for the non-dilated RSU block.

--- u2_net.py
+++ u2_net.py
@@ -1,22 +1,25 @@
 import jax
 import typing as t
+import jax.numpy as jnp
 
 import flax.linen as nn
+
 
 class ConvBNRelu(nn.Module):
     out: int
     kernel_size: int
+    dilation: int = 1
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(self.out, self.kernel_size)(x)
+        x = nn.Conv(self.out, self.kernel_size, dilation=self.dilation)(x)
         x = nn.BatchNorm()(x)
         x = nn.relu(x)
 
         return x
 
 
-class DownConvBNRelu(nn.Module):
+class DownSample(nn.Module):
     out: int
     kernel_size: int
     pool_size: t.Tuple[int, int]
@@ -28,7 +31,7 @@ class DownConvBNRelu(nn.Module):
         return x
 
 
-class UpConvBNRelu(nn.Module):
+class UpSample(nn.Module):
     out: int
     kernel_size: int
     factor: t.Tuple[int, int]
@@ -36,8 +39,23 @@ class UpConvBNRelu(nn.Module):
     @nn.compact
     def __call__(self, x):
         B, H, W, C = x.shape
-        x = jax.image.resize(x, (B, H * self.factor[0], W * self.factor[1], C), method="bilinear")
+        x = jax.image.resize(x, (B, H * self.factor[0], W * self.factor[1], C),
+                             method="bilinear")
         x = ConvBNRelu(self.out, self.kernel_size)(x)
+        return x
+
+
+class DilatedConvBN(nn.Module):
+    out: int
+    kernel_size: int
+    dilation: int
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(self.out, self.kernel_size)(x)
+        x = nn.BatchNorm()(x)
+        x = nn.relu(x)
+
         return x
 
 
@@ -52,9 +70,38 @@ class RSUBlock(nn.Module):
     levels: int
     in_features: int
     out_features: int
+    kernel_size: int
+    m: int
 
     @nn.compact
     def __call__(self, x):
-        pass
+        top_left = ConvBNRelu(self.out_features, self.kernel_size)(x)
 
+        down_levels = [ConvBNRelu(self.m, self.kernel_size)] + [
+            DownSample(self.m, self.kernel_size, (2, 2))
+            for _ in range(self.levels - 2)
+        ]
+
+        bottom = ConvBNRelu(self.m, self.kernel_size, self.dilation)
+
+        up_levels = [ConvBNRelu(self.m, self.kernel_size)] + [
+            UpSample(self.m, self.kernel_size, (2, 2))
+            for _ in range(self.levels - 2)
+        ] + [UpSample(self.out_features, self.kernel_size, (2, 2))]
+
+
+        down_stack = []
+        for layer in down_levels:
+            x = layer(x)
+            down_stack.insert(0, x)
+
+        x = bottom(x)
+
+        for down, layer in enumerate(up_levels):
+            x = jnp.concatenate([down, x], axis=1)
+            x = layer(x)
+
+        out = top_left + x
+
+        return out
 

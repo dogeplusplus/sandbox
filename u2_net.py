1c9f51010fb5919e87dc3e5cb90ad85c2367b3f3
Albert Chung <albertswchung@gmail.com>
2022-04-06 21:38:39 +0100

    Add unit test for bottom dilated RSU block.

--- u2_net.py
+++ u2_net.py
@@ -46,25 +46,22 @@ class U2Net(nn.Module):
 
 class RSUBlock(nn.Module):
     levels: int
-    in_features: int
     out_features: int
     kernel: int
     m: int
     running_avg: bool = False
     dilation: int = 2
-    factor: int = 2
 
     @nn.compact
     def __call__(self, x):
         down_levels = [
-            ConvBNRelu(self.m, self.kernel)
+            ConvBNRelu(self.m, self.kernel, self.running_avg)
             for _ in range(self.levels-1)
         ]
 
-        bottom = ConvBNRelu(self.m, self.kernel, self.running_avg, self.dilation)
 
         up_levels = [
-            ConvBNRelu(self.m, self.kernel)
+            ConvBNRelu(self.m, self.kernel, self.running_avg)
             for _ in range(self.levels - 1)
         ]
 
@@ -81,7 +78,7 @@ class RSUBlock(nn.Module):
         # Insert another convolution without the pooling at the bottom
         down_stack.insert(0, ConvBNRelu(self.m, self.kernel, self.running_avg)(x))
 
-        x = bottom(x)
+        x = ConvBNRelu(self.m, self.kernel, self.running_avg, self.dilation)(x)
 
         for down, layer in zip(down_stack, up_levels):
             x = jnp.concatenate([down, x], axis=-1)
@@ -89,6 +86,7 @@ class RSUBlock(nn.Module):
             B, H, W, C = x.shape
             x = jax.image.resize(x, (B, H * 2, W * 2, C), method="bilinear")
 
+        # Final convolution at the top right before concatenation
         x = ConvBNRelu(self.out_features, self.kernel, self.running_avg)(x)
         out = top_left + x
 
@@ -96,7 +94,6 @@ class RSUBlock(nn.Module):
 
 
 class DilationRSUBlock(nn.Module):
-    in_features: int
     out_features: int
     kernel: int
     m: int
@@ -117,7 +114,7 @@ class DilationRSUBlock(nn.Module):
         u4 = ConvBNRelu(self.m, self.kernel, self.running_avg, dilation=4)(jnp.concatenate([d4, b], axis=-1))
         u3 = ConvBNRelu(self.m, self.kernel, self.running_avg, dilation=4)(jnp.concatenate([d3, u4], axis=-1))
         u2 = ConvBNRelu(self.m, self.kernel, self.running_avg, dilation=2)(jnp.concatenate([d2, u3], axis=-1))
-        u1 = ConvBNRelu(self.m, self.kernel, self.running_avg)(jnp.concatenate([d1, u2], axis=-1))
+        u1 = ConvBNRelu(self.out_features, self.kernel, self.running_avg)(jnp.concatenate([d1, u2], axis=-1))
 
         out = top_left + u1
         return out
@@ -140,16 +137,29 @@ def test_conv_block():
 
 def test_rsu_block():
     levels = 2
-    in_features = 3
     out_features= 5
     kernel = (3,3)
     m = 16
 
     x = jnp.ones((4, 128, 128, 3))
-    block = RSUBlock(levels, in_features, out_features, kernel, m)
+    block = RSUBlock(levels, out_features, kernel, m)
     key = random.PRNGKey(0)
     params = block.init(key, x)
 
     y, _ = block.apply(params, x, mutable=["batch_stats"])
 
     assert y.shape == (4, 128, 128, out_features)
+
+def test_dilation_rsu_block():
+    out_features= 6
+    kernel = (3,3)
+    m = 16
+
+    x = jnp.ones((4, 32, 32, 3))
+    block = DilationRSUBlock(out_features, kernel, m)
+    key = random.PRNGKey(0)
+    params = block.init(key, x)
+
+    y, _ = block.apply(params, x, mutable=["batch_stats"])
+
+    assert y.shape == (4, 32, 32, out_features)

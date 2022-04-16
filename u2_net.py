49537a6d4f78ee1ececeda51a423decad601f073
Albert Chung <albertswchung@gmail.com>
2022-04-16 19:45:22 +0100

    Switch to layer normalization, add augmentation, add model serialization.

--- u2_net.py
+++ u2_net.py
@@ -1,5 +1,6 @@
 import jax
 import optax
+import pickle
 import typing as t
 import jax.numpy as jnp
 import flax.linen as nn
@@ -19,7 +20,7 @@ def upsample(x: jnp.ndarray, factor: int) -> jnp.ndarray:
     return x
 
 
-class ConvBNRelu(nn.Module):
+class ConvLNRelu(nn.Module):
     out: int
     kernel: int
     inference: bool = False
@@ -28,7 +29,8 @@ class ConvBNRelu(nn.Module):
     @nn.compact
     def __call__(self, x):
         x = nn.Conv(self.out, self.kernel, kernel_dilation=self.dilation)(x)
-        x = nn.BatchNorm(use_running_average=self.inference)(x)
+        # x = nn.BatchNorm(use_running_average=self.inference)(x)
+        x = nn.LayerNorm()(x)
         x = nn.relu(x)
 
         return x
@@ -44,16 +46,16 @@ class RSUBlock(nn.Module):
     @nn.compact
     def __call__(self, x):
         down_levels = [
-            ConvBNRelu(self.mid, self.kernel, self.inference)
+            ConvLNRelu(self.mid, self.kernel, self.inference)
             for _ in range(self.levels - 1)
         ]
 
         up_levels = [
-            ConvBNRelu(self.mid, self.kernel, self.inference)
+            ConvLNRelu(self.mid, self.kernel, self.inference)
             for _ in range(self.levels - 1)
         ]
 
-        top_left = ConvBNRelu(self.out, self.kernel, self.inference)(x)
+        top_left = ConvLNRelu(self.out, self.kernel, self.inference)(x)
 
         x = top_left
         down_stack = []
@@ -63,9 +65,9 @@ class RSUBlock(nn.Module):
             x = nn.max_pool(x, (2, 2), (2, 2))
 
         # Insert another convolution without the pooling at the bottom
-        down_stack.insert(0, ConvBNRelu(self.mid, self.kernel, self.inference)(x))
+        down_stack.insert(0, ConvLNRelu(self.mid, self.kernel, self.inference)(x))
 
-        x = ConvBNRelu(self.mid, self.kernel, self.inference, 2)(x)
+        x = ConvLNRelu(self.mid, self.kernel, self.inference, 2)(x)
 
         for down, layer in zip(down_stack, up_levels):
             x = jnp.concatenate([down, x], axis=-1)
@@ -73,7 +75,7 @@ class RSUBlock(nn.Module):
             x = upsample(x, 2)
 
         # Final convolution at the top right before concatenation
-        x = ConvBNRelu(self.out, self.kernel, self.inference)(x)
+        x = ConvLNRelu(self.out, self.kernel, self.inference)(x)
         out = top_left + x
 
         return out
@@ -87,26 +89,26 @@ class DilationRSUBlock(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        top_left = ConvBNRelu(self.out, self.kernel, self.inference)(x)
+        top_left = ConvLNRelu(self.out, self.kernel, self.inference)(x)
 
         x = top_left
-        d1 = ConvBNRelu(self.mid, self.kernel, self.inference)(x)
-        d2 = ConvBNRelu(self.mid, self.kernel, self.inference)(d1)
-        d3 = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=2)(d2)
-        d4 = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=4)(d3)
+        d1 = ConvLNRelu(self.mid, self.kernel, self.inference)(x)
+        d2 = ConvLNRelu(self.mid, self.kernel, self.inference)(d1)
+        d3 = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=2)(d2)
+        d4 = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=4)(d3)
 
-        b = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=8)(d4)
+        b = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=8)(d4)
 
-        u4 = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=4)(
+        u4 = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=4)(
             jnp.concatenate([d4, b], axis=-1)
         )
-        u3 = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=4)(
+        u3 = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=4)(
             jnp.concatenate([d3, u4], axis=-1)
         )
-        u2 = ConvBNRelu(self.mid, self.kernel, self.inference, dilation=2)(
+        u2 = ConvLNRelu(self.mid, self.kernel, self.inference, dilation=2)(
             jnp.concatenate([d2, u3], axis=-1)
         )
-        u1 = ConvBNRelu(self.out, self.kernel, self.inference)(
+        u1 = ConvLNRelu(self.out, self.kernel, self.inference)(
             jnp.concatenate([d1, u2], axis=-1)
         )
 
@@ -119,9 +121,9 @@ class SideSaliency(nn.Module):
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(1, (1, 1))(x)
-        x = jax.nn.sigmoid(x)
+        x = nn.Conv(1, (3, 3))(x)
         x = jax.image.resize(x, self.target_shape, method="bilinear")
+        x = jax.nn.sigmoid(x)
 
         return x
 
@@ -196,7 +198,7 @@ def test_conv_block():
     kernel = (3, 3)
 
     x = jnp.ones((4, 128, 128, 3))
-    layer = ConvBNRelu(out, kernel)
+    layer = ConvLNRelu(out, kernel)
     key = random.PRNGKey(0)
     params = layer.init(key, x)
 
@@ -315,6 +317,56 @@ def duts_dataset(
     val_ds = ds.take(val_size)
     train_ds = ds.skip(val_size)
 
+    augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+        tf.keras.layers.RandomRotation(0.2),
+    ])
+    train_ds = train_ds.map(augmentation)
+
+    train_ds = (
+        train_ds.shuffle(shuffle_buffer, reshuffle_each_iteration=True)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+
+    val_ds = (
+        val_ds.shuffle(shuffle_buffer, reshuffle_each_iteration=True)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+    return train_ds, val_ds
+
+def duts_dataset_original_paper(
+    img_dir: Path,
+    label_dir: Path,
+    batch_size: int,
+    val_ratio: float = 0.2,
+    shuffle_buffer: int = 8,
+):
+
+    norm = lambda x: (x - 0.485) / 0.229
+
+    images = tf.data.Dataset.list_files(str(img_dir / "*"), shuffle=False)
+    labels = images.map(
+        lambda x: tf.strings.regex_replace(x, str(img_dir), str(label_dir))
+    )
+
+    images = images.map(parse_image)
+    images = images.map(lambda x: norm(x))
+
+    labels = labels.map(lambda x: tf.strings.regex_replace(x, ".jpg", ".png"))
+    labels = labels.map(lambda x: parse_image(x, 1))
+    labels = labels.map(lambda x: normalize_image(x, 0, 1))
+
+    ds = tf.data.Dataset.zip((images, labels))
+    size = len(ds)
+    val_size = int(size * val_ratio)
+
+    val_ds = ds.take(val_size)
+    train_ds = ds.skip(val_size)
+
     train_ds = (
         train_ds.shuffle(shuffle_buffer, reshuffle_each_iteration=True)
         .batch(batch_size)
@@ -328,7 +380,6 @@ def duts_dataset(
 
     return train_ds, val_ds
 
-
 def bce_loss(preds: jnp.ndarray, labels: jnp.ndarray) -> float:
     EPS = 1e-8
     preds = jnp.clip(preds, EPS, 1 - EPS)
@@ -336,7 +387,7 @@ def bce_loss(preds: jnp.ndarray, labels: jnp.ndarray) -> float:
     return loss
 
 
-if __name__ == "__main__":
+def main():
     img_dir = Path("..", "..", "Downloads", "DUTS-TR", "DUTS-TR-Image")
     label_dir = Path("..", "..", "Downloads", "DUTS-TR", "DUTS-TR-Mask")
 
@@ -344,7 +395,7 @@ if __name__ == "__main__":
     train_writer = tf.summary.create_file_writer("logs/train")
     valid_writer = tf.summary.create_file_writer("logs/valid")
 
-    batch_size = 16
+    batch_size = 8
     train_ds, val_ds = duts_dataset(img_dir, label_dir, batch_size)
     sample_train_img, sample_train_lab = next(iter(train_ds))
     sample_val_img, sample_val_lab = next(iter(val_ds))
@@ -353,7 +404,7 @@ if __name__ == "__main__":
     mid = 16
     out = 64
     kernel = (3, 3)
-    log_every = 10
+    log_every = 1
 
     x = jnp.zeros((2, 320, 320, 3))
     model = U2Net(mid, out, kernel)
@@ -397,49 +448,47 @@ if __name__ == "__main__":
         tf.summary.image("labels", sample_val_lab, step=0, max_outputs=8)
 
     train_jax_img = jnp.asarray(sample_train_img)
-    train_jax_lab = jnp.asarray(sample_train_lab)
     val_jax_img = jnp.asarray(sample_val_img)
-    val_jax_lab = jnp.asarray(sample_val_lab)
 
     for e in range(epochs):
         train_step = 0
         val_step = 0
-        metrics_dict = defaultdict(lambda: 0)
-        train_bar = tqdm(
-            train_ds, total=len(train_ds), ncols=0, desc=f"Train Epoch {e}"
-        )
+        train_metrics = defaultdict(lambda: 0)
+        val_metrics = defaultdict(lambda: 0)
+        train_bar = tqdm(train_ds, total=len(train_ds), ncols=0, desc=f"Train Epoch {e}")
+        val_bar = tqdm(val_ds, total=len(val_ds), ncols=0, desc=f"Valid Epoch {e}")
 
         for xs, ys in train_bar:
             xs = jnp.asarray(xs)
             ys = jnp.asarray(ys)
 
             params, opt_state, loss = update(params, opt_state, xs, ys)
-            metrics_dict["train_loss"] = (
-                train_step * metrics_dict["train_loss"] + loss
-            ) / (train_step + 1)
+            train_metrics["loss"] = (train_step * train_metrics["loss"] + loss) / (train_step + 1)
 
-            train_bar.set_postfix(**metrics_dict)
+            train_bar.set_postfix(**train_metrics)
             train_step += 1
 
-        val_bar = tqdm(val_ds, total=len(val_ds), ncols=0, desc=f"Valid Epoch {e}")
         for xs, ys in val_bar:
             xs = jnp.asarray(xs)
             ys = jnp.asarray(ys)
 
             loss = loss_fn(params, xs, ys)
-            metrics_dict["val_loss"] = (val_step * metrics_dict["val_loss"] + loss) / (
+            val_metrics["loss"] = (val_step * val_metrics["loss"] + loss) / (
                 val_step + 1
             )
 
-            val_bar.set_postfix(**metrics_dict)
+            val_bar.set_postfix(**val_metrics)
             val_step += 1
 
         with train_writer.as_default():
-            tf.summary.scalar("loss", metrics_dict["train_loss"], step=e)
+            tf.summary.scalar("loss", train_metrics["loss"], step=e)
         with valid_writer.as_default():
-            tf.summary.scalar("loss", metrics_dict["val_loss"], step=e)
+            tf.summary.scalar("loss", val_metrics["loss"], step=e)
 
         if e % log_every == 0:
+            pickle.dump(params, open("weights.pkl", "wb"))
+            pickle.dump(opt_state, open("optimizer.pkl", "wb"))
+
             with train_writer.as_default():
                 pred_train = inference_model.apply(params, train_jax_img)[..., [0]]
                 tf.summary.image("predictions", pred_train, step=e, max_outputs=8)
@@ -450,3 +499,7 @@ if __name__ == "__main__":
 
         train_writer.flush()
         valid_writer.flush()
+
+
+if __name__ == "__main__":
+    main()

d3362f9ad5b34dd7156ffe99b0413b60f6b698d6
Albert Chung <albertswchung@gmail.com>
2022-04-23 18:39:02 +0100

    More GNNs stuff.

--- gnns.py
+++ gnns.py
@@ -145,6 +145,138 @@ print("inp", node_feats)
 print("out", out_feats)
 
 import torch_geometric
+import torch_geometric.nn as geom_nn
+import torch_geometric.data as geom_data
 
 cora_dataset = torch_geometric.datasets.Planetoid(root=DATASET_PATH, name="Cora")
 print(cora_dataset[0])
+
+gnn_layer_by_name = {
+    "GCN": geom_nn.GCNConv,
+    "GAT": geom_nn.GATConv,
+    "GraphConv": geom_nn.GraphConv,
+}
+
+class GNNModel(nn.Module):
+    def __init__(self, c_in, c_hidden, c_out, num_layers=2, layer_name="GCN", dp_rate=0.1, **kwargs):
+        super().__init__()
+        gnn_layer = gnn_layer_by_name[layer_name]
+
+        layers = []
+        in_channels, out_channels = c_in, c_hidden
+
+        for l_idx in range(num_layers-1):
+            layers += [
+                gnn_layer(in_channels=in_channels,
+                          out_channels=out_channels,
+                          **kwargs),
+                nn.ReLU(inplace=True),
+                nn.Dropout(dp_rate)
+            ]
+            in_channels = c_hidden
+        layers += [gnn_layer(in_channels=in_channels,
+                             out_channels=c_out,
+                             **kwargs)]
+        self.layers = nn.ModuleList(layers)
+
+    def forward(self, x, edge_index):
+        for l in self.layers:
+            if isinstance(l, geom_nn.MessagePassing):
+                x = l(x, edge_index)
+            else:
+                x = l(x)
+
+        return x
+
+class MLPModel(nn.Module):
+    def __init__(self, c_in, c_hidden, c_out, num_layers=2, dp_rate=0.1):
+        super().__init__()
+        layers = []
+        in_channels, out_channels = c_in, c_hidden
+        for l_idx in range(num_layers-1):
+            layers += [
+                nn.Linear(in_channels, out_channels),
+                nn.ReLU(inplace=True),
+                nn.Dropout(dp_rate),
+            ]
+            in_channels = c_hidden
+        layers += [nn.Linear(in_channels, c_out)]
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x, *args, **kwargs):
+        return self.layers(x)
+
+class NodeLevelGNN(pl.LightningModule):
+    def __init__(self, model_name, **model_kwargs):
+        super().__init__()
+        self.save_hyperparamters()
+
+        if model_name == "MLP":
+            self.model = MLPModel(**model_kwargs)
+        else:
+            self.model = GNNModel(**model_kwargs)
+
+        self.loss_module = nn.CrossEntropyLoss()
+
+    def forward(self, data, mode="train"):
+        x, edge_index = data.x, data.edge_index
+        x = self.model(x, edge_index)
+
+        if mode == "train":
+            mask = data.train_mask
+        elif mode == "val":
+            mask = data.val_mask
+        elif mode == "test":
+            mask = data.test_mask
+        else:
+            raise Exception("Unknown mode")
+
+        loss = self.loss_module(x[mask], data.y[mask])
+        acc = (x[mask].argmax(dim=-1) == data.y[mask]).sum().float() / mask.sum()
+        return loss, acc
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(self.parameters(), lr=0.1, momentum=0.9, weight_decay=2e-3)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        loss, acc = self.forward(batch, mode="train")
+        self.log("train_loss", loss)
+        self.log("train_acc", acc)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        _, acc = self.forward(batch, mode="val")
+        self.log("val_acc", acc)
+
+    def test_step(self, batch, batch_idx):
+        _, acc = self.forward(batch, mode="test")
+        self.log("test_acc", acc)
+
+
+def train_node_classifier(model_name, dataset, **model_kwargs):
+    pl.seed_everything(42)
+    node_data_loader = geom_data.DataLoader(dataset, batch_size=1)
+
+    root_dir = os.path.join(CHECKPOINT_PATH, "NodeLevel" + model_name)
+    os.makedirs(root_dir, exist_ok=True)
+    trainer = pl.Trainer(default_root_dir=root_dir,
+                         callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc")],
+                         gpus=0,
+                         max_epochs=200,
+                         progress_bar_refresh_rate=0)
+    trainer.logger._default_hp_metric = None
+
+    pretrained_filename = os.path.join(CHECKPOINT_PATH, f"NodeLevel{model_name}.ckpt")
+    if os.path.isfile(pretrained_filename):
+        model = NodeLevelGNN.load_from_checkpoint(pretrained_filename)
+    else:
+        pl.seed_everything()
+        model = NodeLevelGNN(model_name=model_name, c_in=dataset.num_node_features, c_out=dataset.num_classes, **model_kwargs)
+        trainer.fit(model, node_data_loader, node_data_loader)
+        model = NodeLevelGNN.load_from_checkpoint(trainer.checkpoint_callback.best_model_path)
+
+    test_result = trainer.test(model, test_dataloaders=node_data_loader, verbose=False)
+    batch = next(iter(node_data_loader))
+

6936567b101907b8a720cb47914e264d5a1192ed
Albert Chung <albertswchung@gmail.com>
2022-04-29 23:20:52 +0100

    more practice.

--- df_practice.py
+++ df_practice.py
@@ -32,5 +32,31 @@ series_unflattened = pd.Series([["Red", "Green", "White"], ["Red", "Black"], ["Y
 series = series_unflattened.apply(pd.Series).stack().reset_index(drop=True)
 
 series = pd.Series([100, 200, "python", 300.12, 400])
-sorted_series = series.astype(object).sort_values()
-series.sort_values(key = lambda col: col.apply(lambda x: str(x)))
+series = series.sort_values(key = lambda col: col.apply(lambda x: str(x)))
+
+series = pd.Series([100, 200, "python", 300.12, 400])
+series = pd.concat([series, pd.Series([500, "php"])])
+
+series = pd.Series(np.arange(10))
+series_filtered = series[series <= 5]
+
+x = pd.Series([1, 2, 3, 4, 5], index=["A", "B", "C", "D", "E"])
+x = x.reindex(["B", "A", "C", "D", "E"])
+
+x = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 5, 3])
+mu = x.mean()
+std = x.std()
+
+x = pd.Series([1, 2, 3, 4, 5])
+y = pd.Series([2, 4, 6, 8, 10])
+z = x[~x.isin(y)]
+w = pd.concat([x[~x.isin(y)], y[~y.isin(x)]])
+
+x = pd.Series([1, 2, 3, 4, 5, 6, 7, 8])
+y = x.describe()
+
+series = pd.Series([1, 7, 1, 6, 9, 1, 2, 9, 1, 2, 9, 2, 9, 0, 0])
+counts = series.value_counts()
+
+series[series != series.mode()[0]] = "Other"
+import pdb; pdb.set_trace()
